a0ef8af43330d733915956810614407287806c6d
Keerthi Vardhan <keerthivardhantekulapelli@gmail.com>
2023-12-31 11:05:25 +0530

    added pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center text-xl">
+    <div className="flex justify-center items-center text-xl flex-col">
       {/* <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p> */}
       <h1>Hello</h1>
-      <div>
-        <button>Home</button>
-        <button>News</button>
-        <button>Create</button>
-        <button>Contact</button>
+      <div className="flex gap-3 ">
+        <Link  href='/pages/home'>Home</Link>
+        <Link href='/pages/news'>News</Link>
+        <Link href='/pages/create'>Create</Link>
+        <Link href='/pages/contact'>Contact</Link>
       </div>
     </div>
   );
---
+++ app/pages/create/page.tsx
@@ -0,0 +1,9 @@
+
+export default function create() {
+    console.log('====================================');
+    console.log("Rendering create page");
+    console.log('====================================');
+     return (
+    <div>create</div>
+  )
+}
